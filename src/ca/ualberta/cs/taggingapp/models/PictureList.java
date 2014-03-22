0195025ae0660560cd39ad6edb9873867a8dc4a8
Wyatt Johnson <wyatt@wyattjohnson.ca>
2014-03-21 18:59:02 -0600

    Added more methods (setters/getters) to PictureList

--- src/ca/ualberta/cs/taggingapp/models/PictureList.java
+++ src/ca/ualberta/cs/taggingapp/models/PictureList.java
@@ -2,6 +2,10 @@ package ca.ualberta.cs.taggingapp.models;
 
 import java.util.ArrayList;
 
+/**
+ * @author Wyatt Johnson
+ */
+
 public class PictureList {
 	private static PictureList singleton = null;
 	private ArrayList<Picture> pictureList;
@@ -17,5 +21,32 @@ public class PictureList {
 		return singleton;
 	}
 	
+	/*
+	 * Inserts the picture at the top of the list
+	 */
+	public void addPicture(Picture thePicture) {
+		this.pictureList.add(0, thePicture);
+	}
+	
+	/*
+	 * Gets a picture from a specific position in the list
+	 */
+	public Picture getPicture(int position) {
+		return this.pictureList.get(position);
+	}
+	
+	/*
+	 * Sets the picture list to the input
+	 */
+	public void setPictureList(ArrayList<Picture> thePictureList) {
+		this.pictureList = thePictureList;
+	}
+	
+	/*
+	 * Gets the current picture list
+	 */
+	public ArrayList<Picture> getPictureList() {
+		return this.pictureList;
+	}
 	
 }
