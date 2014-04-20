2056eca358536613a4b6fc3b85e22b69b21e860d
Wyatt Johnson <wyatt@wyattjohnson.ca>
2014-04-19 21:06:41 -0600

    Comment managedlist

--- src/ca/ualberta/cs/taggingapp/models/ManagedList.java
+++ src/ca/ualberta/cs/taggingapp/models/ManagedList.java
@@ -2,6 +2,13 @@ package ca.ualberta.cs.taggingapp.models;
 
 import java.util.ArrayList;
 
+/**
+ * A smart object list for sorting elements
+ * 
+ * @author wyatt
+ *
+ * @param <T>
+ */
 public class ManagedList<T extends ManagedObject> {
 
 	protected ArrayList<T> theList;
@@ -10,6 +17,10 @@ public class ManagedList<T extends ManagedObject> {
 		this.theList = new ArrayList<T>();
 	}
 
+	/**
+	 * @param theId
+	 * @return the object of Id
+	 */
 	public T get(String theId) {
 		for (T object : this.theList) {
 			if (object.getId().equals(theId)) {
@@ -19,25 +30,45 @@ public class ManagedList<T extends ManagedObject> {
 		return null;
 	}
 
+	/**
+	 * Gets the element at a position
+	 * @param position
+	 * @return
+	 */
+	public T get(int position) {
+		return this.theList.get(position);
+	}
+
+	/**
+	 * @return the managed list
+	 */
 	public ArrayList<T> getArrayList() {
 		return theList;
 	}
 
+	/**
+	 * Sets the list to the new list
+	 * @param theNewList
+	 */
 	public void setArrayList(ArrayList<T> theNewList) {
 		this.theList.clear();
 		this.theList.addAll(theNewList);
 	}
 
+	/**
+	 * Adds the object to the list
+	 * @param object
+	 */
 	public void add(T object) {
 		this.theList.add(object);
 	}
 
+	/**
+	 * Removes the object from the list
+	 * @param object
+	 */
 	public void remove(T object) {
 		this.theList.remove(object);
-	}
-
-	public T get(int position) {
-		return this.theList.get(position);
 	}
 
 }
